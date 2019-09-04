6351f95f569037ad8421b597bc8d7f31c901d4f3
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-04 05:38:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,9 @@
 def square_array(array)
-  [1, 4, 9]
-  counter = 0
- 
-  while array[counter] do
-    puts array[counter]
-    counter += 1
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
   end
-  
-  puts sqare_array(array)**
-  
+  squared
 end
